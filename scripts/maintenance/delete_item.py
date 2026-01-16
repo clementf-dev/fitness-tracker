25a825bc6c0f6ce40d3a836ecd486d3f8b74912a
Clement <clement.favresse@gmail.com>
2026-01-16 04:46:45 +0100

    Fix script path

--- scripts/maintenance/delete_item.py
+++ scripts/maintenance/delete_item.py
@@ -1,5 +1,11 @@
 import os
+import sys
 import django
+
+# Add project root to sys.path to fix "ModuleNotFoundError"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))
+sys.path.append(project_root)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_tracker.settings')
 django.setup()
